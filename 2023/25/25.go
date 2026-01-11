cef2e026fa45001882ce46d368a4cb7c9cbce8c0
LucasGdosR <lucas.gribel.reis@gmail.com>
2026-01-11 17:04:28 -0300

    23-25 MT

--- 2023/25/25.go
+++ 2023/25/25.go
@@ -3,7 +3,10 @@ package main
 import (
 	"aoc/2023/common"
 	"bufio"
+	"context"
 	"math/rand"
+	"runtime"
+	"slices"
 	"strings"
 )
 
@@ -17,57 +20,113 @@ type unionFind struct {
 }
 
 func main() {
-	G, E := makeGraph()
-	println(kargerMinCut(G, E))
+	thisProgram := common.Benchmarkee[int, int]{
+		ST_Impl:  splitGraphST,
+		MT_Impl:  splitGraphMT,
+		Part1Str: "Componentes product",
+		Part2Str: "nil",
+	}
+	common.Benchmark(thisProgram, 1000)
 }
 
-func kargerMinCut(G graph, E []edge) int {
+func splitGraphST() common.Results[int, int] {
+	G, E := makeGraph()
+
+	rng := rand.New(rand.NewSource(0))
 	n := len(G)
 	uf := unionFind{
 		parents: make([]int, n),
 		sizes:   make([]int, n),
 	}
+
 	for {
-		// Init uf.
-		uf.components = n
-		for i := range n {
-			uf.parents[i] = i
-			uf.sizes[i] = 1
+		if result := kargerMinCut(G, E, uf, rng); result != 0 {
+			return common.Results[int, int]{Part1: result}
 		}
-		// Randomize E.
-		rand.Shuffle(len(E), func(i, j int) {
-			E[i], E[j] = E[j], E[i]
-		})
-
-		// Random contractions.
-		for _, e := range E {
-			uf.union(e[0], e[1])
-			if uf.components == 2 {
-				break
-			}
-		}
-
-		// Correctness check.
-		var cutEdges int
-		for _, e := range E {
-			if uf.find(e[0]) != uf.find(e[1]) {
-				cutEdges++
-			}
-		}
-		if cutEdges != 3 {
-			continue
-		}
-
-		componentSize := make(map[int]int, 2)
-		for i := range n {
-			componentSize[uf.find(i)]++
-		}
-		result := 1
-		for _, v := range componentSize {
-			result *= v
-		}
-		return result
 	}
+}
+
+func splitGraphMT() common.Results[int, int] {
+	G, E := makeGraph()
+
+	numWorkers := runtime.GOMAXPROCS(0)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	resultChan := make(chan int, 1)
+
+	for i := range numWorkers {
+		go func() {
+			rng := rand.New(rand.NewSource(int64(i)))
+			ECopy := slices.Clone(E)
+			n := len(G)
+			uf := unionFind{
+				parents: make([]int, n),
+				sizes:   make([]int, n),
+			}
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+
+				if result := kargerMinCut(G, ECopy, uf, rng); result != 0 {
+					select {
+					case resultChan <- result:
+						return
+					case <-ctx.Done():
+						break
+					}
+				}
+			}
+		}()
+	}
+
+	return common.Results[int, int]{Part1: <-resultChan}
+}
+
+func kargerMinCut(G graph, E []edge, uf unionFind, rng *rand.Rand) int {
+	n := len(G)
+	// Init uf.
+	uf.components = n
+	for i := range n {
+		uf.parents[i] = i
+		uf.sizes[i] = 1
+	}
+	// Randomize E.
+	rng.Shuffle(len(E), func(i, j int) {
+		E[i], E[j] = E[j], E[i]
+	})
+
+	// Random contractions.
+	for _, e := range E {
+		uf.union(e[0], e[1])
+		if uf.components == 2 {
+			break
+		}
+	}
+
+	// Correctness check.
+	var cutEdges int
+	for _, e := range E {
+		if uf.find(e[0]) != uf.find(e[1]) {
+			cutEdges++
+		}
+	}
+	if cutEdges != 3 {
+		return 0
+	}
+
+	var sizeA, sizeB int
+	rootA := uf.find(0)
+	for i := range n {
+		if uf.find(i) == rootA {
+			sizeA++
+		} else {
+			sizeB++
+		}
+	}
+	return sizeA * sizeB
 }
 
 func (uf *unionFind) find(x int) int {
--- 2023/25/README.md
+++ 2023/25/README.md
@@ -2,4 +2,6 @@
 
 Parsing in parallel would be awful, requiring merging the graph later. Better to parse it in a single thread.
 
-Karger's min-cut algorithm is sequential, but since it might need to run multiple times (min-cut is not guaranteed), it is possible to run multiple instances in parallel, and send on a channel to signal that some worker has already found the optimal solution, so everyone can stop. This would require cloning the slice of edges for every worker, although they can share the graph. I did not implement it, so... TODO: implement multiple workers running on different randomized instances and communicating through a channel for termination.
+Karger's min-cut algorithm is sequential, but since it might need to run multiple times (min-cut is not guaranteed), it is possible to run multiple instances in parallel, and send on a channel to signal that some worker has already found the optimal solution, so everyone can stop. This would require cloning the slice of edges for every worker, although they can share the graph. Each goroutine also has its private union-find.
+
+We could you a channel for signalling the work is done, but using context is the standard way of cancelling work, so that's what I used. Sometimes the multithreaded code is ~20% faster, but it's slower on average. We could batch multiple runs of Karger's algorithm to try and find the sweet spot where MT is usually faster for a serious application.
