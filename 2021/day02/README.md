12ac56309a5d5fff5d307e3b515ac4c97e5e2037
LucasGdosR <lucas.gribel.reis@gmail.com>
2026-02-16 15:37:44 -0300

    profile 21/02

--- 2021/day02/README.md
+++ 2021/day02/README.md
@@ -1,16 +1,20 @@
 # Benchmark
 
 Part 1:
-- 781 ns/op
+- 691 ns/op
 - 0 B/op
 
 Part 2:
-- 826 ns/op
+- 767 ns/op
 - 0 B/op
 
 # Optimizations
 
 All we need to parse is the first character in each line, then we know which command it is, what's the offset for the number, which is always 1 digit, and the index for the next line. Read just the needed bytes rather than the whole input.
+
+For part 1, we can keep a count of how many '0' adjustments to make rather than parsing the actual input for every line, and then apply the adjustment just once.
+
+Instead of using a switch statement, we can use if-statements ordered by the most common case.
 
 This is embarrassingly parallel for part 1, but sequential for part 2. There's not enough work to amortize spawning goroutines and reducing for part 1.
 
--- 2021/day02/solution.go
+++ 2021/day02/solution.go
@@ -15,21 +15,25 @@ func Part1(input []byte) int {
 }
 
 func part1(input []byte) (int, int) {
-	var X, Y int
+	var X, Y, dx, dy int
 	for i := 0; i < len(input); {
-		switch input[i] {
-		case 'f':
-			X += int(input[i+8]) - '0'
+		c := input[i]
+		// Faster than switch
+		if c == 'f' {
+			X += int(input[i+8])
+			dx++
 			i += 10
-		case 'd':
-			Y += int(input[i+5]) - '0'
+		} else if c == 'd' {
+			Y += int(input[i+5])
+			dy++
 			i += 7
-		case 'u':
-			Y -= int(input[i+3]) - '0'
+		} else {
+			Y -= int(input[i+3])
+			dy--
 			i += 5
 		}
 	}
-	return X, Y
+	return X - dx*'0', Y - dy*'0'
 }
 
 func Part2(input []byte) int {
@@ -40,16 +44,16 @@ func Part2(input []byte) int {
 func part2(input []byte) (int, int) {
 	var X, Y, aim int
 	for i := 0; i < len(input); {
-		switch input[i] {
-		case 'f':
+		c := input[i]
+		if c == 'f' {
 			v := int(input[i+8]) - '0'
 			X += v
 			Y += v * aim
 			i += 10
-		case 'd':
+		} else if c == 'd' {
 			aim += int(input[i+5]) - '0'
 			i += 7
-		case 'u':
+		} else {
 			aim -= int(input[i+3]) - '0'
 			i += 5
 		}
